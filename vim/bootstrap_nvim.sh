30b327dbc078700e5f8ab6332fdae5c79cc7d297
Juliane Klatt <juliane.klatt@bsse.ethz.ch>
2020-03-05 12:07:48 +0100

    added Bastian's bootstrap magic

---
+++ vim/bootstrap_nvim.sh
@@ -0,0 +1,6 @@
+python3 -m venv $HOME/.virtualenvs/nvim
+source $HOME/.virtualenvs/nvim/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade neovim
+pip3 install --upgrade pynvim
+pip3 install --upgrade "python-language-server[all]"
